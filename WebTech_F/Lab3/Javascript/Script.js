91e4a38a546646b2c9deb0e02e0f9a11f9db2d9a
RifatRudro <rifat.rudro138964@gmail.com>
2026-02-22 11:45:53 +0600

    JS Intro With Collect Data

---
+++ WebTech_F/Lab3/Javascript/Script.js
@@ -0,0 +1,32 @@
+console.log("Connected");
+//alert("HTML are Connected");
+// let a = 20.56;
+// let b = 40;
+// let c = a+b;
+// console.log("Sum of C is",c);
+// if(a==0)
+// {
+//     a++;
+//     console.log("Increment Values",a);
+// }
+// else if(a>20 && a<25){
+//     b = a+b;
+//     console.log("Current Value of B:",b);
+// }
+// for(let i=0;i<5; i++)
+// {
+//     console.log(i);
+// }
+function collet_data()
+{
+let name = document.getElementById("PatientName").value;
+console.log(name);
+
+let PatientAge = document.getElementById("Age").value;
+console.log(PatientAge);
+
+let DateOfBirth = document.getElementById("DOB").value;
+console.log(DateOfBirth); 
+
+return false;
+}
