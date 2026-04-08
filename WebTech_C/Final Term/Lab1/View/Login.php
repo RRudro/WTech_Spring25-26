67359ec39541f23dbd265a918d1b8356f10528b2
RifatRudro <rifat.rudro138964@gmail.com>
2026-04-08 11:39:58 +0600

    Refactor login page: update form structure and remove session error handling

--- WebTech_C/Final Term/Lab1/View/Login.php
+++ WebTech_C/Final Term/Lab1/View/Login.php
@@ -1,49 +1,40 @@
-<!DOCTYPE html>
-<?php
-session_start();
-$Error = $_SESSION["usernameError"] ?? "";
-unset($_SESSION["usernameError"]);
-?>
+<!DOCTYPE html> 
 <html>
     <body>
-        <form method = "post" action ="../Controller/LoginValidation.php" >
+        <?php
+        echo "<h1> This is a log in Form </h1>";
+        ?> 
+        <form>
             <table>
-                <tr>
-                    <td>
-                        <label> User Name: </label>
-                    </td>
-                    <td>
-                        <input type = "text" name = "username"/>
-                    </td>
+                <tr> 
+                    <td> <p> User Name: </p> </td>
+                    <td> <input type= "text"/> </td>
                 </tr>
                 <tr>
-                    <td>
-                        <label> Password: </label>
-                    </td>
-                    <td>
-                        <input type = "password" name = "password"/>
-                    </td>
+                    <td> <p> Password: </p></td>
+                    <td> <input type ="password"/> </td>
                 </tr>
                 <tr>
-                    <td> 
-
-                    </td>
-                    <td>
-                        <?php
-                        echo "<p style = 'color: red'>$Error </p>";
-                        ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td> 
-
-                    </td>
-                    <td> 
-                        <input type ="submit" name="submit"/>
-                    </td>
+                    <td> <input type = "submit"/> </td>
                 </tr>
             </table>
         </form>
+    <?php
+    echo "<h1 style = 'color: red'>Hello World</h1>";
+    $text1 = "Hello php";
+    $text2 = "Web Tech";
+    echo "<h1> php Initiated $text1 </h1>";
+    ?>
+    <?php
+    // $variable1= 10.5;
+    // $variable2= 20;
+    // echo $variable1 + $variable2;
+    // $cars = array("WebTech", "C#", "OOP1");
+    // var_dump($cars);
+    $car = array("Course"=>"WebTech", "Section"=> "C", "ClassTime"=> "10.20-12.20");
+    echo $car["Section"];
+    ?> 
+
 
     </body>
-</html>/
+</html>
