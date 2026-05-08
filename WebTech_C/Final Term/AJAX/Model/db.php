e5ca7549838bd7c3b05a14c69bbe13ee91f20769
RRudro <mamun.rudro@aiub.edu>
2026-05-08 12:52:35 +0600

    Refactor signup function to use prepared statements for SQL injection prevention

--- WebTech_C/Final Term/AJAX/Model/db.php
+++ WebTech_C/Final Term/AJAX/Model/db.php
@@ -36,6 +36,15 @@ function CheckUserName($connection, $tablename, $username)
     return $result;
 }
 
+function WithSQLInjection($connection, $tablename, $username, $password, $filepath)
+{
+    $sql= "INSERT INTO " .$tablename. "(username, password, filepath) VALUES (?,?,?)";
+    $statement=$connection->prepare($sql);
+    $statement->bind_param("sss",$username, $password,$filepath);
+    $result = $statement->execute();
+    return $result;
+}
+
 
 
 }
